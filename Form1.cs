8c3e87d37b405684ac997b07b148da5612675131
Михайло Кондратенко <mike.kondr@gmail.com>
2023-11-03 09:43:44 +0200

    first change

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace PhotoOrg
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
     }
 }
